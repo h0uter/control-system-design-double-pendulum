c84a896a034f80f2273b7e596fe7d631ffe5b0e2
Robert Rooijmans <rrooijmans@tudelft.net>
2020-02-26 13:19:13 +0100

    Up to date

--- measureAngles.m
+++ measureAngles.m
@@ -1,9 +1,8 @@
 function measureAngles = measureAngles() 
-%% TODO! --> IK KAN DIT HELAAS NIET VANAF THUIS DOEN / TESTEN.
-block1 = 'rotpentemplate/Theta1';
+block1 = 'PhysicalParts/rotpentemplate/Theta1';
 rto = get_param(block1, 'RuntimeObject');
 Theta1 = rto.InputPort(1).Data;
-block2 = 'rotpentemplate/Theta2';
+block2 = 'PhysicalParts/rotpentemplate/Theta2';
 rto = get_param(block2, 'RuntimeObject');
 Theta2 = rto.InputPort(1).Data;
 measureAngles = [Theta1,Theta2];
