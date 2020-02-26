24d71f4a6339e89667665f0a06f7783d950da3b1
Robert Rooijmans <rrooijmans@tudelft.net>
2020-02-26 14:03:32 +0100

    Calibreren

--- PhysicalParts/hwinit.m
+++ PhysicalParts/hwinit.m
@@ -7,8 +7,8 @@ daoutoffs = [0.00];                   % output offset
 daoutgain = 1*[-6];                   % output gain
 
 % Sensor calibration:
-adinoffs = [ 0.1644    -1.2449];
-adingain = [1.1733    1.2526];
+adinoffs = [-6.3703   -1.2582];
+adingain = [1.2465    1.2559];
 
 adinoffs = [adinoffs 0 0 0 0 0];    % input offset
 adingain = [adingain 1 1 1 1 1];     % input gain (to radians)
