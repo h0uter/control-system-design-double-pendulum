d8437019f226fcd2f0ba9892cb76a1432ecbfc30
cgrain <coengruijt@gmail.com>
2020-02-28 16:34:07 +0100

    of toch niet

--- Pendulum Model/CoenParams.m
+++ Pendulum Model/CoenParams.m
@@ -1,8 +1,21 @@
-function [P1, P2, P3, g1, g2] = CoenParams(X) 
+function Parameters = CoenParams(X) 
+Parameters.g = X(1);
+Parameters.l1 = X(2);
+Parameters.l2 = X(3);
+Parameters.m1 = X(4);
+Parameters.m2 = X(5);
+Parameters.c1 = X(6); 
+Parameters.c2 = X(7);
+Parameters.I1 = X(8);
+Parameters.I2 = X(9);
+Parameters.b1 = X(10);
+Parameters.b2 = X(11);
+Parameters.km = X(12);
+Parameters.Te = X(13);
 % X = [g, l1, l2, m1, m2, c1, c2, I1, I2, b1, b2, km, Te];
-P1 = X(4)*X(6)^2 + X(5)*X(2)^2 + X(8);
-P2 = X(5)*X(7)^2 + X(9);
-P3 = X(5)*X(2)*X(7);
-g1 = X(1)*(X(4)*X(6)+X(5)*X(2));
-g2 = X(5)*X(7)*X(1);
+Parameters.P1 = X(4)*X(6)^2 + X(5)*X(2)^2 + X(8);
+Parameters.P2 = X(5)*X(7)^2 + X(9);
+Parameters.P3 = X(5)*X(2)*X(7);
+Parameters.g1 = X(1)*(X(4)*X(6)+X(5)*X(2));
+Parameters.g2 = X(5)*X(7)*X(1);
 end
