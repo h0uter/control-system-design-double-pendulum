c66f7be241f27ea83bdc8947021ffbdcdb5206f3
cgrain <coengruijt@gmail.com>
2020-02-29 15:52:50 +0100

    woutmodel updated

--- Pendulum Model/LSQnonLinfunc.m
+++ Pendulum Model/LSQnonLinfunc.m
@@ -1,4 +1,4 @@
-function err = LSQnonLinfunc(X, Realydata) 
+function err = LSQnonLinfunc(X, Realydata, Input) 
 % X = [ g, l1, l2, m1, m2, c1, c2, I1, I2,b1, b2, km, Te]
 Parameters = CoenParams(X);
 simOut = sim('woutModel', 'SrcWorkspace', 'current');
--- Pendulum Model/LSQtest.m
+++ Pendulum Model/LSQtest.m
@@ -1,12 +1,17 @@
+
 %% Simulate Data;
 X = [9.81, 0.1, 0.1, 0.125, 0.05, -0.04, 0.06, 0.074, 0.00012, 4.8, 0.0002, 50, 0.03];
 Parameters = CoenParams(X);
-out = sim('woutModel');
-Realydata = out.Theta.data;
+tout = linspace(0,10,1000);
+Input = [tout', 0.3*ones(1000,1)];
+% RealData = sim('RealModel');
+% Input = RealData.Input; % Or something like that. 
+outModel = sim('woutModel');
+Realydata = outModel.Theta.data;
 Parameters.km = 0;
 %% LSQnonlin
-func = @(x) LSQnonLinfunc(selectParam(x),Realydata);
+func = @(x) LSQnonLinfunc(selectParam(x),Realydata, Input);
 
-X0 = [0.12, 0.04, 4.7, 0.0001];
+X0 = [0.08, 0.04, 4.7, 0.0001];
 options = optimoptions('lsqnonlin', 'Display', 'iter');
 x = lsqnonlin(func,X0,[],[], options)
