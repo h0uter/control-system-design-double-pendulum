ea6ad700f2e62e583e803aaa1ac6ca366e1eddb1
cgrain <coengruijt@gmail.com>
2020-02-29 15:41:12 +0100

    LSQnonlin werkt met model wouter + fake data

--- Pendulum Model/LSQnonLinfunc.m
+++ Pendulum Model/LSQnonLinfunc.m
@@ -1,7 +1,6 @@
 function err = LSQnonLinfunc(X, Realydata) 
 % X = [ g, l1, l2, m1, m2, c1, c2, I1, I2,b1, b2, km, Te]
 Parameters = CoenParams(X);
-simOut = sim('Level2Model', 'SrcWorkspace', 'current');
-ydata = simOut.Output2.data;
-ydata(5);
+simOut = sim('woutModel', 'SrcWorkspace', 'current');
+ydata = simOut.Theta.data;
 err = ydata - Realydata;
--- Pendulum Model/LSQtest.m
+++ Pendulum Model/LSQtest.m
@@ -1,10 +1,12 @@
 %% Simulate Data;
-Parameters.km = 2;
-out = sim('Level2Model');
-Realydata = out.Output2.data;
+X = [9.81, 0.1, 0.1, 0.125, 0.05, -0.04, 0.06, 0.074, 0.00012, 4.8, 0.0002, 50, 0.03];
+Parameters = CoenParams(X);
+out = sim('woutModel');
+Realydata = out.Theta.data;
 Parameters.km = 0;
 %% LSQnonlin
-func = @(x) LSQnonLinfunc(x,Realydata);
-x0 = 1;
+func = @(x) LSQnonLinfunc(selectParam(x),Realydata);
+
+X0 = [0.12, 0.04, 4.7, 0.0001];
 options = optimoptions('lsqnonlin', 'Display', 'iter');
-x = lsqnonlin(func,x0,[],[], options)
+x = lsqnonlin(func,X0,[],[], options)
