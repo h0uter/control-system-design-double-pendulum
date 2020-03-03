4e43afbbf2d3055fa765060f497103b751962eba
cgrain <coengruijt@gmail.com>
2020-03-03 14:32:19 +0100

    Update 33

--- Pendulum Model/TestModel.m
+++ Pendulum Model/TestModel.m
@@ -1,4 +1,12 @@
-X0 = [9.8100,    0.0991,    0.1000 ,   0.1251,    0.0490,   -0.0405,    0.0600,    0.0434,   0.00012    ,6.2420, ...
-    0.0002,   49.8465,    0.03];
+YBoundData = load('../measurements/bound1_constantTorque_3.mat');
+YBound = [YBoundData.Theta1.data, YBoundData.Theta2.data];
+tout = linspace(0,10000,1000000);
+Input = [tout', 0.0*ones(1000000,1)];
+initSpeed = 1;
+X0 = [9.8100,    0.0991,    0.1000 ,   0.1251,    0.050,   -0.040,    0.0600,    0.0744,   0.00012    ,4.8, ...
+    0.0002,   -50,    0.0307];
 Parameters = CoenParams(X0);
-sim('woutModel');
+output1 = sim('boundedModel');
+thetad = output1.Theta_d.data;
+theta = output1.Theta.data;
+[thetad, theta]
