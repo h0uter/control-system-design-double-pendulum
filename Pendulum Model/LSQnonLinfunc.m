b05a851cb20a32438f89d7b93e234d1fcf3df262
cgrain <coengruijt@gmail.com>
2020-03-03 13:12:26 +0100

    push all

--- Pendulum Model/LSQnonLinfunc.m
+++ Pendulum Model/LSQnonLinfunc.m
@@ -1,6 +1,10 @@
-function err = LSQnonLinfunc(X, Realydata, Input) 
+function err = LSQnonLinfunc(X, Realydata, Input, bounded) 
 % X = [ g, l1, l2, m1, m2, c1, c2, I1, I2,b1, b2, km, Te]
 Parameters = CoenParams(X);
-simOut = sim('woutModel', 'SrcWorkspace', 'current');
+if (bounded)
+simOut = sim('boundedModel', 'SrcWorkspace', 'current');
+else 
+    simOut = sim('woutModel', 'SrcWorkspace', 'current');
+end
 ydata = simOut.Theta.data;
 err = ydata - Realydata;
