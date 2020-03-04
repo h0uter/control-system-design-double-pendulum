a170f9873af117630552ff7536c186860130f20b
Robert Rooijmans <rrooijmans@tudelft.net>
2020-03-04 14:52:23 +0100

    BlackBox

--- measurements/blackbox/signalblackbox.m
+++ measurements/blackbox/signalblackbox.m
@@ -1,6 +1,5 @@
-time=100;
-signal.time = linspace(0,100,10000);
-signal.values = 0.1*ones(1,time/h);
-lengthinput = 10;
-ampl=1;
-signal2 = [linspace(0,100,10000/lengthinput)',  -0.5+ampl*round(rand(1,time/(h*lengthinput)))'];
+time=20;
+lengthinput = 8;
+ampl=0.3;
+signal2 = [linspace(0,time,time*100/lengthinput)',  -0.15+ampl*round(rand(1,time/(h*lengthinput)))'];
+sim('ParamEstimate');
