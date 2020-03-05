2e48f54a102621b8f0b41be4c6a0aa8f51322dc1
Robert Rooijmans <rrooijmans@tudelft.net>
2020-03-05 14:52:19 +0100

    bb 2 datasets

--- measurements/blackbox/signalblackbox.m
+++ measurements/blackbox/signalblackbox.m
@@ -1,5 +1,10 @@
-time=20;
+h=0.01;
+time=10;
 lengthinput = 8;
-ampl=0.3;
-signal2 = [linspace(0,time,time*100/lengthinput)',  -0.15+ampl*round(rand(1,time/(h*lengthinput)))'];
-sim('ParamEstimate');
+ampl=0.4;
+signal2 = [linspace(0,time,time*100/lengthinput)',  -1/2*ampl+ampl*round(rand(1,time/(h*lengthinput)))'];
+mean(signal2(:,2))
+signal2=signal2 - mean(signal2(:,2));
+%%
+sim('ParamEstimatebb');
+save('C:\Users\rrooijmans\Github\CSL-ROT2.1-CoenRobWout\measurements\blackbox\test.mat')
