cc8b7a9bb85a8fcdf8d11a84c5f6a1d8ee9c724c
robertrooijmans <r.w.f.rooijmans@student.tudelft.nl>
2020-03-28 16:28:08 +0100

    plotting of controller results + disturbance added again

--- Pendulum Model/testingreftrackingLQR.m
+++ Pendulum Model/testingreftrackingLQR.m
@@ -1,19 +1,21 @@
 %% LOAD PARAM
 clear all;
 Parameters = EstimatedParams();
-Parameters.disturbance = 1;
 h=0.00002;
 close all
 
 %% TRIAL AND ERROR
-K2 =  [0.0849 2.6780 2.4547 -0.3203 0.1625]; %from testing polecontroller.m
+w = [4, 4, 3.75, 3]; % away from frequencies from the system. 
+zeta = [ 0.6, 0.65, 0.7, 1.1];
+PoleFunc = @(w,zeta) -w.*zeta+ [w.*sqrt(zeta.^2-1),-w.*sqrt(zeta.^2-1) ];
+P2 = [PoleFunc(w(3),zeta(3)), -50.0, -60.0, -100.0];
 A = Parameters.LinTopA;
 B = Parameters.LinTopB;
 C = Parameters.LinTopXY;
 D = [ 0 ; 0];
 sys = ss(A, B, C,D);
-%P2 =  -[5, 12, 50, 60, 100];
 %K2 = place(A, B, P2);
+K2 = [-0.0148 8.7739 7.3655 1.3853 0.6376];
 %% CHeck for closed loop eigenvalues
 Ac2 = A - B*K2;
 Ec2 = eig(Ac2);
@@ -23,6 +25,11 @@ figure(1)
 impulse(sysCL2);
 figure(2)
 step(sysCL2);
+Kdc = dcgain(sysCL2);
+Kr = 1/Kdc(1);
+sysCL2_scaled = ss(Ac2, B*Kr, C, D);
+figure(4)
+step(sysCL2_scaled);
 %% RETRIEVING DATA FROM NL MODEL
 firstLocation = [0 0];
 firstLocationFull = [ 0 , firstLocation, 0, 0];
@@ -37,13 +44,19 @@ Error = ERRORVAR;
 Input = [linspace(0,time,lengthSpace)', 0*ones(1,lengthSpace)'];
 %% OBSERVE BEST MODEL
 t=linspace(0,10,10/h);
+Reference = [t;zeros(1,length(t));0.3*sin(t);-0.3*sin(t);zeros(1,length(t));zeros(1,length(t))]';
+Parameters.Lff = [0;Kr;0;0;0]';
 Parameters.PoleGain = K2;
-Test2 = sim('LinearTopTest');
+Test2 = sim('reftracking_LinearTopTest');
+timesim=linspace(0,10,length(Test2.Theta_Model.data(:,2:3)));
 
 %% plot
 figure(3)
-plot(Test2.sim_time.data,Test2.Theta_Model.data(:,2:3))
+plot(timesim,Test2.Theta_Model.data(:,2:3))
+hold on
+ref = 0.3*sin(timesim);
+plot(timesim,ref)
 legend('Theta1','Theta2','Reference');
 xlabel('Time [s]');
 ylabel('Angle [rad]');
-title('Stabilizing pole controller after disturbance of second link at t = 1 second')
+title('Reference tracking pole controller')
