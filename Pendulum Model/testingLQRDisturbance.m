e9d1d87ce7efd513521b82731d6c74c3b01c65ad
robertrooijmans <r.w.f.rooijmans@student.tudelft.nl>
2020-03-30 13:34:13 +0200

    cleaned up .m files and changed disturbance to 25 instead of 30 and added simtime to reftracking slx

--- Pendulum Model/testingLQRDisturbance.m
+++ Pendulum Model/testingLQRDisturbance.m
@@ -1,49 +1,30 @@
-%% LOAD PARAM
+%% Load parameters
 clear all;
-Parameters = EstimatedParams();
-Parameters.disturbance = 1;
+Parameters = EstimatedParams();         %use estimated parameters
+Parameters.disturbance = 1;             %toggle on disturbance in NLmodel.slx. See matlab function 4 block in NLmodel for disturbance specification
 h=0.00002;
 close all
 
-%% TRIAL AND ERROR
-K2 =  [-0.0148 8.7739 7.3655 1.3853 0.6376]; %from testing LQR.m
+%% Define controller gains and statespace model parameters for simulation
+K =  [-0.0148 8.7739 7.3655 1.3853 0.6376];    %pole gains determined from testing LQR.m
 A = Parameters.LinTopA;
 B = Parameters.LinTopB;
 C = Parameters.LinTopXY;
 D = [ 0 ; 0];
 sys = ss(A, B, C,D);
-%P2 =  -[5, 12, 50, 60, 100];
-%K2 = place(A, B, P2);
-%% CHeck for closed loop eigenvalues
-Ac2 = A - B*K2;
-Ec2 = eig(Ac2);
-%% Make Systems+Controller + design reference gain
-sysCL2 = ss(Ac2, B, C, D);
-figure(1)
-impulse(sysCL2);
-figure(2)
-step(sysCL2);
-%% RETRIEVING DATA FROM NL MODEL
+
+%% Initialize position
 firstLocation = [0 0];
 firstLocationFull = [ 0 , firstLocation, 0, 0];
-time=10;
-lengthinput = 8;
-lengthSpace = int32(time/(h*lengthinput));
-ampl=0.005;
-ERRORVAR = [linspace(0,time,lengthSpace)',  -1/2*ampl+ampl*round(rand(1,lengthSpace))', ...
-     -1/2*ampl+ampl*round(rand(1,lengthSpace))', -1/2*ampl+ampl*round(rand(1,lengthSpace))', ...
-     -1/2*ampl+ampl*round(rand(1,lengthSpace))', -1/2*ampl+ampl*round(rand(1,lengthSpace))'];
-Error = ERRORVAR;
-Input = [linspace(0,time,lengthSpace)', 0*ones(1,lengthSpace)'];
-%% OBSERVE BEST MODEL
-t=linspace(0,10,10/h);
-Parameters.PoleGain = K2;
-Test2 = sim('LinearTopTest');
+
+%% Simulation
+Parameters.PoleGain = K;
+Test = sim('LinearTopTest');
 
 %% plot
-figure(3)
-plot(Test2.sim_time.data,Test2.Theta_Model.data(:,2:3))
+figure(1)
+plot(Test.sim_time.data,Test.Theta_Model.data(:,2:3))
 legend('Theta1','Theta2','Reference');
 xlabel('Time [s]');
 ylabel('Angle [rad]');
-title('Stabilizing pole controller after disturbance of second link at t = 1 second')
+title('Stabilizing LQR controller after disturbance of second link at t = 1 second')
