4446f80c7cbea33b67d3c51cd08375ba515ba3a4
cgrain <coengruijt@gmail.com>
2020-03-30 15:32:45 +0200

    Pullen werkt niet in matlab, dan mag je het daarna zelf commiten

--- Pendulum Model/testingreftracking_pole.m
+++ Pendulum Model/testingreftracking_pole.m
@@ -4,57 +4,36 @@ Parameters = EstimatedParams();
 h=0.00002;
 close all
 
-%% TRIAL AND ERROR
-w = [4, 4, 3.75, 3]; % away from frequencies from the system. 
-zeta = [ 0.6, 0.65, 0.7, 1.1];
-PoleFunc = @(w,zeta) -w.*zeta+ [w.*sqrt(zeta.^2-1),-w.*sqrt(zeta.^2-1) ];
-P2 = [PoleFunc(w(3),zeta(3)), -50.0, -60.0, -100.0];
+%% Define controller gains and statespace model parameters for simulation
+K =  [0.0849 2.6780 2.4547 -0.3203 0.1625]; %pole gains determined form testingpolecontroller.m
 A = Parameters.LinTopA;
 B = Parameters.LinTopB;
 C = Parameters.LinTopXY;
-D = [ 0 ; 0];
+D = [0;0];
 sys = ss(A, B, C,D);
-K2 = place(A, B, P2);
-%% CHeck for closed loop eigenvalues
-Ac2 = A - B*K2;
-Ec2 = eig(Ac2);
-%% Make Systems+Controller + design reference gain
-sysCL2 = ss(Ac2, B, C, D);
-figure(1)
-impulse(sysCL2);
-figure(2)
-step(sysCL2);
-Kdc = dcgain(sysCL2);
+
+%% Design reference gain
+Ac2 = A - B*K;
+sysCL = ss(Ac2, B, C, D);
+Kdc = dcgain(sysCL);
 Kr = 1/Kdc(1);
-sysCL2_scaled = ss(Ac2, B*Kr, C, D);
-figure(4)
-step(sysCL2_scaled);
-%% RETRIEVING DATA FROM NL MODEL
+
+%% Initialize position
 firstLocation = [0 0];
 firstLocationFull = [ 0 , firstLocation, 0, 0];
-time=10;
-lengthinput = 8;
-lengthSpace = int32(time/(h*lengthinput));
-ampl=0.005;
-ERRORVAR = [linspace(0,time,lengthSpace)',  -1/2*ampl+ampl*round(rand(1,lengthSpace))', ...
-     -1/2*ampl+ampl*round(rand(1,lengthSpace))', -1/2*ampl+ampl*round(rand(1,lengthSpace))', ...
-     -1/2*ampl+ampl*round(rand(1,lengthSpace))', -1/2*ampl+ampl*round(rand(1,lengthSpace))'];
-Error = ERRORVAR;
-Input = [linspace(0,time,lengthSpace)', 0*ones(1,lengthSpace)'];
+
 %% OBSERVE BEST MODEL
 t=linspace(0,10,10/h);
 Reference = [t;zeros(1,length(t));0.3*sin(t);-0.3*sin(t);zeros(1,length(t));zeros(1,length(t))]';
 Parameters.Lff = [0;Kr;0;0;0]';
-Parameters.PoleGain = K2;
-Test2 = sim('reftracking_LinearTopTest');
-timesim=linspace(0,10,length(Test2.Theta_Model.data(:,2:3)));
+Parameters.PoleGain = K;
+Test = sim('reftracking_LinearTopTest');
 
 %% plot
-figure(3)
-plot(timesim,Test2.Theta_Model.data(:,2:3))
+figure(1)
+plot(Test.sim_time.data , Test.Theta_Model.data(:,2:3))
 hold on
-ref = 0.3*sin(timesim);
-plot(timesim,ref)
+plot(Reference(:,1),Reference(:,3))
 legend('Theta1','Theta2','Reference');
 xlabel('Time [s]');
 ylabel('Angle [rad]');
